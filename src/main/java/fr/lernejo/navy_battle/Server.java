a0e53f1661e273091cc3492a3734d04b8fa5a601
Theophile GREGOIRE <theophile.gregoire@icloud.com>
2021-06-23 15:28:42 +0200

    partie 2 - add test stop

--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -6,20 +6,37 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.http.HttpClient;
 import java.util.concurrent.Executors;
 
-public class Server{
+public class Server {
+    static HttpServer server;
+
     public void startServer(int port) throws IOException {
         var server = HttpServer.create(new InetSocketAddress(port), 0);
+        Server.server = server;
         server.setExecutor(Executors.newSingleThreadExecutor());
         server.createContext("/ping", Server::pingHandler);
         server.start();
-}
-static void pingHandler(HttpExchange exchange) throws IOException{
-    String body = "OK";
-    exchange.sendResponseHeaders(200, body.length());
-    try (OutputStream os = exchange.getResponseBody()) { // (1)
-        os.write(body.getBytes());
     }
+
+    public void startGame(){
+        Server.server.createContext("/api/game/start", Server::startGameHandler);
+    }
+
+    private static void startGameHandler(HttpExchange httpExchange) {
+
+    }
+
+    public void stopServer() {
+        Server.server.stop(0);
+    }
+
+    static void pingHandler(HttpExchange exchange) throws IOException {
+        String body = "Hello";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
+            os.write(body.getBytes());
+        }
     }
 }
--- src/test/java/fr/lernejo/navy_battle/ServerTest.java
+++ src/test/java/fr/lernejo/navy_battle/ServerTest.java
@@ -1,5 +1,6 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpServer;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -7,15 +8,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.Delayed;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ServerTest {
     @Test
-    public void testping() throws IOException {
+    public void startServerTest() throws IOException {
         new Server().startServer(9876);
         String url = "http://localhost:9876/ping";
-        String source ="";
+        String source = "";
         URL oracle = new URL(url);
         URLConnection yc = oracle.openConnection();
         BufferedReader in = new BufferedReader(
@@ -23,8 +26,25 @@ public class ServerTest {
                 yc.getInputStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null)
-            source +=inputLine;
+            source += inputLine;
         in.close();
-        assertEquals("OK", source);
+        assertEquals("Hello", source);
+    }
+
+    @Test
+    public void stopServerTest() throws IOException {
+        Server server = new Server();
+        server.startServer(9999);
+        server.stopServer();
+        String url = "http://localhost:9999/ping";
+        URL oracle = new URL(url);
+        URLConnection yc = oracle.openConnection();
+        try{
+            new InputStreamReader(
+                yc.getInputStream());
+        }catch(Exception e){
+            System.out.print(e);
+            assertTrue(true);
+        }
     }
 }
