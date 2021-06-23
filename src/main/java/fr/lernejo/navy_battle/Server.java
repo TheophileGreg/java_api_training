901c4d12a63c6c5314bf8a408c09760e5855bf81
Theophile GREGOIRE <theophile.gregoire@icloud.com>
2021-06-23 11:19:51 +0200

    config server - is ok

---
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -0,0 +1,10 @@
+package fr.lernejo.navy_battle;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+
+public class Server{
+    public void startServer(int port) throws IOException {
+System.out.print("mescouilles" + port);
+}
+}
