2f381efdb2bceeb548677f222d976b536abd6cae
Theophile GREGOIRE <theophile.gregoire@icloud.com>
2021-07-11 00:50:43 +0200

    change port on server test

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
 
     public static void main(String[] args) {
         try {
-            if (args.length != 1) {
+            if (args.length == 0) {
                 System.err.println("Need [port] argument ans maybe {server_url}");
                 System.exit(-1);
             }
